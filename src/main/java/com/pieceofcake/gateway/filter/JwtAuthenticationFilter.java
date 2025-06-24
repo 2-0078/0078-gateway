18410a6900785ec0968d588b5e39310f148d0ce4
JungDongSeop <ds123911@gmail.com>
2025-06-24 13:19:20 +0900

    fix: WHITE_LIST 추가

--- src/main/java/com/pieceofcake/gateway/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/pieceofcake/gateway/filter/JwtAuthenticationFilter.java
@@ -27,7 +27,8 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
             "/api/v1/check-email",
             "/api/v1/find-email",
             "/api/v1/phone/send-code",
-            "/api/v1/phone/verify"
+            "/api/v1/phone/verify",
+            "/api/v1/money/with-member-uuid"
             // 추가적으로 인증 필요없는 경로들 여기에!
     };
 
