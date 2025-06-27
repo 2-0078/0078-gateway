b0a6564c01b386ef9b5ae4202b5fda429b1fc2bb
Eunseo <kendyjenny@naver.com>
2025-06-27 15:45:11 +0900

    [Fix] : AccessToken WhiteList 수정

--- src/main/java/com/pieceofcake/gateway/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/pieceofcake/gateway/filter/JwtAuthenticationFilter.java
@@ -31,57 +31,46 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
 
             "/api/v1/piece/product/uuid-list",
             "/api/v1/piece/product",
-            "/api/v1/piece/product/{pieceProductUuid}",
             "/api/v1/piece",
-            "/api/v1/piece/delete-all/{productUuid}",
-            "/api/v1/piece/owned/{pieceProductUuid}/list",
+            "/api/v1/piece/delete-all/",
+            "/api/v1/piece/owned/",
 
-            "/api/v1/participation/remain/{fundingUuid}",
+            "/api/v1/participation/remain/",
 
-            "/api/v1/reply/child/{parentReplyUuid}",
-            "/api/v1/reply/list/{boardType}/{boardUuid}",
-            "/api/v1/reply/community/{replyUuid}",
+            "/api/v1/reply/child/",
+            "/api/v1/reply/list/",
+            "/api/v1/reply/community/",
 
             "/api/v1/money/with-member-uuid",
             "/api/v1/brandpay/callback",
 
             "/api/v1/vote",
-            "/api/v1/bid/list/{auctionUuid}",
+            "/api/v1/bid/list/",
             "/api/v1/auction",
-            "/api/v1/auction/sse/price-updates/{auctionUuid}",
-            "/api/v1/auction/highest-price/{auctionUuid}",
+            "/api/v1/auction/sse/price-updates/",
+            "/api/v1/auction/highest-price/",
 
-            "/api/v1/piece/graph/real-time/{pieceProductUuid}",
+            "/api/v1/piece/graph/real-time/",
 
             "/api/v1/board/notice",
-            "/api/v1/board/notice/{boardUuid}",
             "/api/v1/board/faq",
-            "/api/v1/board/faq/{boardUuid}",
             "/api/v1/board/event",
-            "/api/v1/board/event/{boardUuid}",
             "/api/v1/board/community",
 
             "/api/v1/product/list",
-            "/api/v1/product/list/{productUuid}",
             "/api/v1/piece/list",
-            "/api/v1/piece/list/{pieceProductUuid}",
             "/api/v1/funding/list",
-            "/api/v1/funding/list/{fundingUuid}",
 
             "/api/v1/sub-category",
-            "/api/v1/sub-category/{subCategoryId}",
             "/api/v1/sub-category/list",
-            "/api/v1/sub-category/list/{mainCategoryId}",
             "/api/v1/product",
-            "/api/v1/product/{productUuid}",
             "/api/v1/main-category",
-            "/api/v1/main-category/{mainCategoryId}",
             "/api/v1/main-category/list",
             "/api/v1/price",
 
             "/stock",
-            "/api/v1/kis-api/quotes/{pieceProductUuid}",
-            "/api/v1/kis-api/market-price/{pieceProductUuid}"
+            "/api/v1/kis-api/quotes/",
+            "/api/v1/kis-api/market-price/"
     };
 
     public JwtAuthenticationFilter(JwtProvider jwtProvider) {
